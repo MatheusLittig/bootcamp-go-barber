024aaaa647b9ae67c405b930e1d6b9d7368a0ddf
Matheus Littig <littig.works@gmail.com>
2020-11-27 21:58:01 -0400

    repository pattern - appointments methods

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,10 @@
 import express from 'express'
+import routes from './routes/index'
 
 const app = express()
+
+app.use(express.json())
+app.use(routes)
 
 app.get('/', (request, response) => {
   return response.json({ message: 'Hello Word! 🙂 ' })
