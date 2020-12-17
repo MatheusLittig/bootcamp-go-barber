476146928811086c3fb869b94602803e02516073
MatheusLittig <littig.works@gmail.com>
2020-12-16 20:12:30 -0400

    avatar pic replace and static files list access

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,18 +1,16 @@
 import 'reflect-metadata'
+import './database'
 
 import express from 'express'
 import routes from './routes/index'
 
-import './database'
+import uploadConfig from './config/upload'
 
 const app = express()
 
 app.use(express.json())
+app.use('/files', express.static(uploadConfig.directory))
 app.use(routes)
-
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello Word! 🙂 ' })
-})
 
 app.listen(3333, () => {
   console.log('🚀 Server started at on port 3333!')
