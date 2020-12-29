348a2eb02d45a9ae12a7154220c83650d2f8a3f7
Matheus Littig <littig.works@gmail.com>
2020-12-29 14:19:29 -0400

    cors added

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,9 @@
 import 'reflect-metadata'
 import './database'
+import 'express-async-errors'
 
 import express, { Request, Response, NextFunction } from 'express'
-import 'express-async-errors'
+import cors from 'cors'
 
 import routes from './routes/index'
 
@@ -11,6 +12,7 @@ import AppError from './errors/AppError'
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use('/files', express.static(uploadConfig.directory))
 app.use(routes)
